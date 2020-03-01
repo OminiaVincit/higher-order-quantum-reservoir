054bed063db9047cc8d91954157a9cbd540a8fc9
OminiaVincit <k09tranhoan@gmail.com>
2020-03-02 00:11:47 +0900

    Add log axis

--- source/run_mem_func.py
+++ source/run_mem_func.py
@@ -57,61 +57,72 @@ if __name__  == '__main__':
 
     basename = args.basename
     savedir = args.resdir
-    if os.path.isdir(savedir) == False:
-        os.mkdir(savedir)
 
-    qparams = qrc.QRCParams(hidden_unit_count=hidden_unit_count, max_coupling_energy=max_coupling_energy,\
-        trotter_step=trotter_step, beta=beta)
+    if os.path.isfile(savedir):
+        # Load result file and plot
+        rsarr = np.loadtxt(savedir)
+        figbase = savedir.replace('.txt', '')
+    else:
+        if os.path.isdir(savedir) == False:
+            os.mkdir(savedir)
 
-    timestamp = int(time.time() * 1000.0)
-    now = datetime.datetime.now()
-    datestr = now.strftime('{0:%Y-%m-%d-%H-%M-%S}'.format(now))
-    # Multi-process
-    tmpdir = os.path.join(savedir, '{}_{}'.format(basename, timestamp))
-    os.mkdir(tmpdir)
+        qparams = qrc.QRCParams(hidden_unit_count=hidden_unit_count, max_coupling_energy=max_coupling_energy,\
+            trotter_step=trotter_step, beta=beta)
 
-    processes = []
-    lst = np.array_split(range(N), nproc)
-    for proc_id in range(nproc):
-        outlist = lst[proc_id]
-        if outlist.size == 0:
-            continue
-        print(outlist)
-        p = Process(target=memory_compute, args=(outlist, tmpdir, qparams, train_len, val_len, buffer, maxD, proc_id))
-        processes.append(p)
+        timestamp = int(time.time() * 1000.0)
+        now = datetime.datetime.now()
+        datestr = now.strftime('{0:%Y-%m-%d-%H-%M-%S}'.format(now))
+        # Multi-process
+        tmpdir = os.path.join(savedir, '{}_{}'.format(basename, timestamp))
+        os.mkdir(tmpdir)
+
+        processes = []
+        lst = np.array_split(range(N), nproc)
+        for proc_id in range(nproc):
+            outlist = lst[proc_id]
+            if outlist.size == 0:
+                continue
+            print(outlist)
+            p = Process(target=memory_compute, args=(outlist, tmpdir, qparams, train_len, val_len, buffer, maxD, proc_id))
+            processes.append(p)
     
-    # Start the process
-    for p in processes:
-        p.start()
+        # Start the process
+        for p in processes:
+            p.start()
     
-    # Ensure all processes have finiished execution
-    for p in processes:
-        p.join()
+        # Ensure all processes have finiished execution
+        for p in processes:
+            p.join()
 
-    # Sleep 5s
-    time.sleep(5)
+        # Sleep 5s
+        time.sleep(5)
 
-    # Average results and remove tmpdir
-    rsarr = []
-    for idx in range(N):
-        filename = os.path.join(tmpdir, 'mem_idx_{}.txt'.format(idx))
-        if os.path.isfile(filename):
-            arr = np.loadtxt(filename)
-            rsarr.append(arr)
-    rsarr = np.mean(rsarr, axis=0)
+        # Average results and remove tmpdir
+        rsarr = []
+        for idx in range(N):
+            filename = os.path.join(tmpdir, 'mem_idx_{}.txt'.format(idx))
+            if os.path.isfile(filename):
+                arr = np.loadtxt(filename)
+                rsarr.append(arr)
+        rsarr = np.mean(rsarr, axis=0)
 
-    # Save results
-    outbase = os.path.join(savedir, '{}_{}'.format(basename, datestr))
-    np.savetxt('{}_mem.txt'.format(outbase), rsarr, delimiter='\t')
+        # remove tmpdir
+        import shutil
+        shutil.rmtree(tmpdir)
+        
+        # Save results
+        outbase = os.path.join(savedir, '{}_{}'.format(basename, datestr))
+        np.savetxt('{}_mem.txt'.format(outbase), rsarr, delimiter='\t')
     
-    # save experiments setting
-    with open('{}_setting.txt'.format(outbase), 'w') as sfile:
-        sfile.write('train_len={}, val_len={}, buffer={}, maxD={}, numtrials={}\n'.format(train_len, val_len, buffer, maxD, N))
-        sfile.write('hidden_unit_count={}\n'.format(qparams.hidden_unit_count))
-        sfile.write('max_coupling_energy={}\n'.format(qparams.max_coupling_energy))
-        sfile.write('trotter_step={}\n'.format(qparams.trotter_step))
-        sfile.write('beta={}\n'.format(qparams.beta))
-    
+        # save experiments setting
+        with open('{}_setting.txt'.format(outbase), 'w') as sfile:
+            sfile.write('train_len={}, val_len={}, buffer={}, maxD={}, numtrials={}\n'.format(train_len, val_len, buffer, maxD, N))
+            sfile.write('hidden_unit_count={}\n'.format(qparams.hidden_unit_count))
+            sfile.write('max_coupling_energy={}\n'.format(qparams.max_coupling_energy))
+            sfile.write('trotter_step={}\n'.format(qparams.trotter_step))
+            sfile.write('beta={}\n'.format(qparams.beta))
+        figbase = '{}_mem'.format(outbase)
+
     # save MF plot
     plt.style.use('seaborn-colorblind')
     plt.rc('font', family='serif')
@@ -121,10 +132,8 @@ if __name__  == '__main__':
     ax.set_xlabel(r'Delay $d$', fontsize=16)
     ax.set_ylabel(r'$MF_d$', fontsize=16)
     dlist, MFlist = rsarr[:, 0], rsarr[:, 1]
+    ax.set_yscale('log')
     ax.scatter(dlist, MFlist, label='MF_d')
     for ftype in ['png', 'pdf', 'svg']:
-        plt.savefig('{}_mem.{}'.format(outbase, ftype), bbox_inches='tight')
+        plt.savefig('{}.{}'.format(figbase, ftype), bbox_inches='tight')
     
-    # remove tmpdir
-    import shutil
-    shutil.rmtree(tmpdir)
