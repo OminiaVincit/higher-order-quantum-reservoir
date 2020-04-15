Update code to fix calculation of effective dimension

source/cal_eff_dim.py
@@ -14,11 +14,11 @@ import qrc
 import gendata as gen
 import utils
 
-def eff_job(qparams, P, T, input_seq_ls, Ntrials, send_end):
+def eff_job(qparams, buffer, length, P, Ntrials, send_end):
     esp_ls = []
     print('Start process taudelta={}, virtual={}, Jdelta={}'.format(qparams.tau_delta, qparams.virtual_nodes, qparams.max_coupling_energy))
     for n in range(Ntrials):
-         esp_val = qrc.effective_dim(qparams, P, T, input_seq_ls)
+         esp_val = qrc.effective_dim(qparams, buffer, length, ranseed=n, P=P)
          esp_ls.append(esp_val)
 
     mean_esp = np.mean(esp_ls)
@@ -36,15 +36,13 @@ if __name__  == '__main__':
     parser.add_argument('--trotter', type=int, default=10)
     parser.add_argument('--beta', type=float, default=1e-14)
 
-    parser.add_argument('--vallen', type=int, default=500)
+    parser.add_argument('--length', type=int, default=1000)
     parser.add_argument('--buffer', type=int, default=500)
     parser.add_argument('--pindex', type=int, default=10)
     parser.add_argument('--ntrials', type=int, default=1)
     parser.add_argument('--nums', type=int, default=1)
     
     parser.add_argument('--resolution', type=int, default=64)
-
-    parser.add_argument('--orders', type=str, default='10')
     parser.add_argument('--basename', type=str, default='qrc_effective')
     parser.add_argument('--savedir', type=str, default='reseff')
     args = parser.parse_args()
@@ -52,7 +50,7 @@ if __name__  == '__main__':
 
     hidden_unit_count, max_coupling_energy, trotter_step, beta =\
         args.units, args.coupling, args.trotter, args.beta
-    val_len, pindex, buffer = args.vallen, args.pindex, args.buffer
+    length, pindex, buffer = args.length, args.pindex, args.buffer
     Ntrials = args.ntrials
     nums = args.nums
 
@@ -66,16 +64,11 @@ if __name__  == '__main__':
     virtuals = [5*n for n in range(1, nums+1)]
     virtuals.insert(0, 1)
 
-    orders = [int(x) for x in args.orders.split(',')]
-    data, target = gen.make_data_for_narma(buffer + val_len, orders=orders)
-
-    input_seq_ls = np.array(  [ data[: buffer + val_len] ] )
-    
     # Evaluation
     timestamp = int(time.time() * 1000.0)
     now = datetime.datetime.now()
     datestr = now.strftime('{0:%Y-%m-%d-%H-%M-%S}'.format(now))
-    outbase = os.path.join(savedir, '{}_{}_order_{}'.format(basename, datestr, '_'.join([str(o) for o in orders])))
+    outbase = os.path.join(savedir, '{}_{}'.format(basename, datestr))
 
     if True:
         if os.path.isfile(savedir) == False:
@@ -87,7 +80,7 @@ if __name__  == '__main__':
                     qparams = qrc.QRCParams(hidden_unit_count=hidden_unit_count, max_coupling_energy=max_coupling_energy,\
                         trotter_step=trotter_step, beta=beta, virtual_nodes=V, tau_delta=tdelta, init_rho=0)
                     
-                    p = multiprocessing.Process(target=eff_job, args=(qparams, pindex, buffer, input_seq_ls, Ntrials, send_end))
+                    p = multiprocessing.Process(target=eff_job, args=(qparams, buffer, length, pindex, Ntrials, send_end))
                     jobs.append(p)
                     pipels.append(recv_end)
 

source/esp_highorder_tau_change.py
@@ -22,12 +22,12 @@ from loginit import get_module_logger
 # layers = [n for n in range(1, 6)]
 # strengths = [0.0 0.1 0.3 0.5 0.7 0.9 1.0]
 
-def esp_job(qparams, nqrc, layer_strength, buffer, input_seq, state_trials, net_trials, send_end):
+def esp_job(qparams, nqrc, layer_strength, buffer, length, state_trials, net_trials, send_end):
     print('Start process layer={}, taudelta={}, virtual={}, Jdelta={}'.format(nqrc, qparams.tau_delta, qparams.virtual_nodes, qparams.max_coupling_energy))
     btime = int(time.time() * 1000.0)
     dPs, ldas = [], []
     for n in range(net_trials):
-        dP, lda = hqrc.esp_index(qparams, buffer, input_seq, nqrc, layer_strength, ranseed=n, state_trials=state_trials)
+        dP, lda = hqrc.esp_index(qparams, buffer, length, nqrc, layer_strength, ranseed=n, state_trials=state_trials)
         dPs.append(dP)
         ldas.append(lda)
 
@@ -62,8 +62,7 @@ if __name__  == '__main__':
     parser.add_argument('--strength', type=float, default=0.0)
     parser.add_argument('--virtuals', type=int, default=15)
 
-    parser.add_argument('--orders', type=str, default='10')
-    parser.add_argument('--basename', type=str, default='qrc_narma_echo')
+    parser.add_argument('--basename', type=str, default='qrc_echo')
     parser.add_argument('--savedir', type=str, default='res_high_echo_tau')
     args = parser.parse_args()
     print(args)
@@ -84,30 +83,22 @@ if __name__  == '__main__':
     
     layers = [int(x) for x in args.layers.split(',')]
     
-    orders = [int(x) for x in args.orders.split(',')]
-    data, target = gen.make_data_for_narma(length, orders=orders)
-    input_seq_org = np.array(data)
-    input_seq_org = input_seq_org.reshape(1, input_seq_org.shape[0])
-    
-
     # Evaluation
     timestamp = int(time.time() * 1000.0)
     now = datetime.datetime.now()
     datestr = now.strftime('{0:%Y-%m-%d-%H-%M-%S}'.format(now))
-    outbase = os.path.join(savedir, '{}_{}_strength_{}_V_{}_layers_{}_narma_{}_esp_trials_{}_{}'.format(\
-        basename, datestr, layer_strength, V, '_'.join([str(l) for l in layers]), \
-            '_'.join([str(o) for o in orders]), net_trials, state_trials))
+    outbase = os.path.join(savedir, '{}_{}_strength_{}_V_{}_layers_{}_esp_trials_{}_{}'.format(\
+        basename, datestr, layer_strength, V, '_'.join([str(l) for l in layers]), net_trials, state_trials))
     
     if os.path.isfile(savedir) == False:
         jobs, pipels = [], []
         for nqrc in layers:
-            input_seq = np.tile(input_seq_org, (nqrc, 1))
             for tau_delta in taudeltas:
                 recv_end, send_end = multiprocessing.Pipe(False)
                 qparams = qrc.QRCParams(hidden_unit_count=hidden_unit_count, max_coupling_energy=max_coupling_energy,\
                     trotter_step=trotter_step, beta=beta, virtual_nodes=V, tau_delta=tau_delta, init_rho=init_rho)
                 p = multiprocessing.Process(target=esp_job, \
-                    args=(qparams, nqrc, layer_strength, buffer, input_seq, net_trials, state_trials, send_end))
+                    args=(qparams, nqrc, layer_strength, buffer, length, net_trials, state_trials, send_end))
                 jobs.append(p)
                 pipels.append(recv_end)
 

source/highorder_qrc.py
@@ -183,8 +183,7 @@ class HighorderQuantumReservoirComputing(object):
                 self.current_states[i] = np.array(current_state)
                 local_rhos[i] = rho
 
-            # only use state of the last qrc to train
-            state = np.array(self.current_states)
+            state = np.array(self.current_states.copy())
             state_list.append(state.flatten())
 
             # update previous states
@@ -377,13 +376,19 @@ def effective_dim(qparams, buffer, length, nqrc, layer_strength, ranseed, Ntrial
                 locls.append(np.mean(ri*rj))
         locls = np.array(locls).reshape(D, D)
         w, v = LA.eig(locls)
+        #print(w)
         w = np.abs(w) / np.abs(w).sum()
         effdims.append(1.0 / np.power(w, 2).sum())
     return np.mean(effdims), np.std(effdims)
 
-def esp_index(qparams, buffer, input_seq, nqrc, layer_strength, \
-        ranseed, state_trials, one_input=False):
-    input_seq = np.array(input_seq)
+def esp_index(qparams, buffer, length, nqrc, layer_strength, ranseed, state_trials, one_input=False):
+    if ranseed >= 0:
+        np.random.seed(seed=ranseed)
+
+    data = np.random.rand(length)
+    input_seq = np.array(data)
+    input_seq = np.tile(input_seq, (nqrc, 1))
+
     # Initialize the reservoir to zero state - density matrix
     model = HighorderQuantumReservoirComputing(nqrc, layer_strength, one_input)
     x0_state_list = model.init_forward(qparams, input_seq, init_rs = True, ranseed = ranseed)
@@ -392,9 +397,8 @@ def esp_index(qparams, buffer, input_seq, nqrc, layer_strength, \
     for i in range(state_trials):
         # Initialzie the reservoir to a random initial state
         # Keep same coupling configuration
-        
         model.gen_rand_rhos(ranseed = i + 300000)
-        z0_state_list = model.init_forward(qparams, input_seq, init_rs = False, ranseed = ranseed)
+        z0_state_list = model.init_forward(qparams, input_seq, init_rs = False, ranseed = i + 200000)
         L, D = z0_state_list.shape
         # L = Length of time series
         # D = Number of layers x Number of virtual nodes x Number of qubits

source/qrc.py
@@ -353,7 +353,7 @@ def memory_function(taskname, qparams, train_len, val_len, buffer, dlist, ransee
     
     return np.array(list(zip(dlist, MFlist, MFstds, train_list, val_list)))
 
-def esp_index(qparams, P, T, input_seq_ls, ranseed):
+def esp_index(qparams, P, buffer, input_seq_ls, ranseed):
     input_seq_ls = np.array(input_seq_ls)
     
     # Initialzie the reservoir to zero state - density matrix
@@ -385,27 +385,34 @@ def esp_index(qparams, P, T, input_seq_ls, ranseed):
     print('dP={}'.format(dP/P))
     return dP/P
 
-def effective_dim(qparams, P, T, input_seq_ls):
+def effective_dim(qparams, buffer, length, ranseed, P):
     # Calculate effective dimension for reservoir
     from numpy import linalg as LA
 
-    input_seq_ls = np.array(input_seq_ls)
-    model = QuantumReservoirComputing()
+    if ranseed >= 0:
+        np.random.seed(seed=ranseed)
 
+    data = np.random.rand(length)
+    input_seq_ls = np.array([ data ])
+
+    model = QuantumReservoirComputing()
     effdim = []
     for p in range(P):
+        ranseed_net = ranseed
+        if ranseed >= 0:
+            ranseed_net = (ranseed + 11000) * (p + 1)
         corrsum = []
-        state_list = model.init_forward(qparams, input_seq_ls, ranseed=-1, init_rs=True)
+        state_list = model.init_forward(qparams, input_seq_ls, ranseed=ranseed_net, init_rs=True)
         N, L, D = state_list.shape
         # N = Number of input list
         # L = Length of time series
         # D = Number of virtual nodes x Number of qubits
-        for n in range(N):
+        for n in range(1):
             locls = []
             for i in range(D):
                 for j in range(D):
-                    ri = state_list[n, :, i]
-                    rj = state_list[n, :, j]
+                    ri = state_list[n, buffer:, i]
+                    rj = state_list[n, buffer:, j]
                     locls.append(np.mean(ri*rj))
             locls = np.array(locls).reshape(D, D)
             corrsum.append(locls)