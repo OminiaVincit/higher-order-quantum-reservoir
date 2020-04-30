815029e4f0000ad9e734ef4b13f5a9ff547980b6
OminiaVincit <k09tranhoan@gmail.com>
2020-04-30 09:47:08 +0900

    Add results for narma task

--- run_deep_narma.sh
+++ run_deep_narma.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-for a in 0.1 0.3 0.5 0.7 0.9
+
+for a in 5 10 15 20
 do
-for N in 1 2 3 4 5 
-  do
-    python source/narma_nmse_deep.py --strength $a --ntrials 10 --nqrc $N 
-done
+  python source/narma_nmse_highorder_tau_change.py --savedir narma_deep_tau --deep 1 --ntrials 10 --virtuals 15 --layers 2,3,4,5 --strength 0.5
+  python source/narma_nmse_highorder_tau_change.py --savedir narma_deep_tau --deep 1 --ntrials 10 --virtuals 15 --layers 2,3,4,5 --strength 0.9
+  python source/narma_nmse_highorder_strength_change.py --savedir narma_deep_strength --deep 1 --ntrials 10 --virtuals 20 --taudelta 2.0 --layers 2,3,4,5 --strengths 0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0
 done
