9f6705a49ac31c960f7b6e173c035e27b1ce19f4
OminiaVincit <k09tranhoan@gmail.com>
2020-05-02 02:19:22 +0900

    Update plotting esp index

--- plot_ESP_delta.py
+++ plot_ESP_delta.py
@@ -7,25 +7,28 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+from matplotlib import ticker, cm
 
 if __name__  == '__main__':
     # Check for command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--folder', type=str, default='res_high_echo3')
-    parser.add_argument('--prefix', type=str, default='qrc_echo_2020-04')
+    parser.add_argument('--prefix', type=str, default='qrc_echo_2020-0')
     parser.add_argument('--posfix', type=str, default='esp')
+    parser.add_argument('--virtuals', type=str, default='1')
     parser.add_argument('--strengths', type=str, default='0.0,0.9')
+    parser.add_argument('--vmin', type=int, default=-15)
+    parser.add_argument('--vmax', type=int, default=0)
     args = parser.parse_args()
     print(args)
     folder, prefix, posfix = args.folder, args.prefix, args.posfix
     strengths = [float(x) for x in args.strengths.split(',')]
 
-    orders = [5,10,15,20]
     Js = [0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0]
-    Vs = [1, 15]
+    Vs = [int(v) for v in args.virtuals.split(',')]
     slims  = [[1e-8, 2e-1], [2e-6, 1e-1], [2e-5, 1e-1], [2e-4, 1.2]]
     M, N = len(Vs), len(strengths)
-    cmap = plt.get_cmap("viridis")
+    cmap = plt.get_cmap("RdBu")
     fig, axs = plt.subplots(1, N*M, figsize=(5*M*N, 4))
     axs = axs.ravel()
     #plt.style.use('seaborn-colorblind')
@@ -50,23 +53,27 @@ if __name__  == '__main__':
                     ntitle = ntitle.replace('.txt', '')
                     tmp = np.loadtxt(rfile)
                     print(tmp.shape)
-                    rsarr.append(tmp)
+                    rsarr.append(tmp[:, [2, -2, -1]])
                     xs.append(J)
             nx = len(rsarr)
             if len(rsarr) > 0:
                 rsarr = np.concatenate(rsarr, axis=0)
                 print('V={},strength={}'.format(V, alpha), rsarr.shape)
-                ys, avg_esp, std_esp = rsarr[:, 2], rsarr[:, 4], rsarr[:, 5]
+                ys, avg_esp, std_esp = rsarr[:, 0], rsarr[:, 1], rsarr[:, 2]
                 
                 arr_2d = avg_esp.reshape((nx, -1))
                 ny = arr_2d.shape[1]
                 print('nx={}, ny={}'.format(nx, ny))
+                
                 arr_2d = np.log10(arr_2d)
+                #ax.contourf(arr_2d, cmap=cm.RdBu, levels=[10**x for x in np.linspace(-5, 0, 11)])
+
                 df = pd.DataFrame(data=arr_2d, \
-                    index=['{:.0f}'.format(np.log2(J)) for J in xs], \
-                    columns=['{:.0f}'.format(np.log2(t)) for t in ys[:ny]])
+                   index=['{:.0f}'.format(np.log2(J)) for J in xs], \
+                   columns=['{:.0f}'.format(np.log2(t)) for t in ys[:ny]])
             
-                sns.heatmap(data=df, ax=ax, cmap='viridis')
+                sns.heatmap(data=df, ax=ax, cmap=cmap, vmin=args.vmin, vmax=args.vmax)
+
                 # print('xs shape ', len(xs), 'ys shape ', len(ys))
                 # xs = xs.reshape((9, 15))
                 # ys = ys.reshape((9, 15))
@@ -77,6 +84,7 @@ if __name__  == '__main__':
                 #ax.plot(xs, avg_esp, 'o-', alpha = 0.8, linewidth=1.5, markersize=6, mec='k', mew=0.5, label='$J$={}'.format(J))
                 #ax.errorbar(xs, avg_esp, yerr=std_esp, elinewidth=2, linewidth=2, markersize=12, \
                 #    label='J={}'.format(J))
+            ax.set_title('V={},$\\alpha$={}'.format(V,alpha), fontsize=16)
             # ax.set_yscale('log', basey=10)
             # ax.set_xscale('log', basex=2)
             # #ax.set_ylim(slims[i])
@@ -86,7 +94,7 @@ if __name__  == '__main__':
     outbase = '{}\{}'.format(folder, ntitle)
     plt.suptitle(outbase, fontsize=12)
     
-    #for ftype in ['pdf', 'svg']:
-    #    plt.savefig('{}_nmse.{}'.format(outbase, ftype), bbox_inches='tight')
+    for ftype in ['pdf', 'svg']:
+        plt.savefig('{}_esp.{}'.format(outbase, ftype), bbox_inches='tight')
     plt.show()
     
