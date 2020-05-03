4c6f4c5afdd0f2606a2023f9edb0b05e5622bda2
Hoan Tran <zoro@s100.lab>
2020-05-03 18:02:57 +0900

    Add more results for high coupling

--- run_high_capa_coupling.sh
+++ run_high_capa_coupling.sh
@@ -13,6 +13,6 @@ for a in 0.9 0.0 0.5
 do
     for J in 2.0 0.5 4.0 0.25 8.0 0.125 1.0 16.0 0.0625 
     do
-        python $BIN --coupling $J --plot 0 --taskname $TASK --layers $L --strength $a --ntrials $N --nproc $PROC --maxd 200 --savedir $SAVE
+        python $BIN --coupling $J --plot 0 --taskname $TASK --layers $L --strength $a --virtuals $V --ntrials $N --nproc $PROC --maxd 200 --savedir $SAVE
     done
 done
