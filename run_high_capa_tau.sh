f39bdb4a26089b07fbd3245d4ee04f19105ddcea
OminiaVincit <k09tranhoan@gmail.com>
2020-05-03 10:38:40 +0900

    Add results for memory capacity

--- run_high_capa_tau.sh
+++ run_high_capa_tau.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/bash
-python source/mem_capacity_highorder_tau_change.py --taskname qrc_stm --layers 4,5 --strength 0.0 --ntrials 10 --nproc 51 --maxd 200
+BIN=source/mem_capacity_highorder_tau_change.py
+N=10
+V=1
+TASK=qrc_stm
+SAVE=rescapa_high_stm2
+L=2,3,4,5
+python $BIN --plot 1 --strength 0.5 --ntrials $N --virtuals $V --taskname $TASK --layers $L --nproc 51 --maxd 200 --savedir $SAVE
+python $BIN --plot 1 --strength 0.9 --ntrials $N --virtuals $V --taskname $TASK --layers $L --nproc 51 --maxd 200 --savedir $SAVE
