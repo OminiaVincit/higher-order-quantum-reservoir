19e98dc219741ca811277a92691b1b7c906e0c8a
Hoan Tran <zoro@s100.lab>
2020-05-05 10:28:52 +0900

    Add results for memory capacity varying V

--- run_high_capa_V.sh
+++ run_high_capa_V.sh
@@ -9,9 +9,9 @@ SAVE=rescapa_high_V
 L=5
 PROC=90
 
-for a in 0.9 0.0 0.5
+for a in 0.0 0.5
 do
-    for V in 5 10 20 25 30 35 40 45 50 
+    for V in 5 10 20 25 
     do
         python $BIN --coupling $J --plot 0 --taskname $TASK --layers $L --strength $a --virtuals $V --ntrials $N --nproc $PROC --maxd 200 --savedir $SAVE
     done
