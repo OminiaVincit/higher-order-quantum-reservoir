0e912ada277c39b1780e760b1808e7bea833228f
Hoan Tran <zoro@s100.lab>
2020-05-10 09:23:58 +0900

    Add results for high capacity V

--- run_high_capa_V.sh
+++ run_high_capa_V.sh
@@ -7,11 +7,11 @@ J=1.0
 TASK=qrc_stm
 SAVE=rescapa_high_V
 L=5
-PROC=90
+PROC=80
 
-for a in 0.0 0.5
+for a in 0.5
 do
-    for V in 5 10 20 25 
+    for V in 5 10 
     do
         python $BIN --coupling $J --plot 0 --taskname $TASK --layers $L --strength $a --virtuals $V --ntrials $N --nproc $PROC --maxd 200 --savedir $SAVE
     done
