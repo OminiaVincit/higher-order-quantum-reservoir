34a0cb9d5e3571dbc16fb0c91a22c5a2b09525ee
OminiaVincit <k09tranhoan@gmail.com>
2020-05-21 16:53:13 +0900

    add results for capacity changing alpha

--- run_high_capa_tau.sh
+++ run_high_capa_tau.sh
@@ -3,7 +3,8 @@ BIN=source/mem_capacity_highorder_tau_change.py
 N=10
 V=1
 TASK=qrc_stm
-SAVE=rescapa_high_tauf
-L=2,3,4,5
-python $BIN --plot 1 --strength 0.5 --ntrials $N --virtuals $V --taskname $TASK --layers $L --nproc 51 --maxd 200 --savedir $SAVE
-python $BIN --plot 1 --strength 0.9 --ntrials $N --virtuals $V --taskname $TASK --layers $L --nproc 51 --maxd 200 --savedir $SAVE
+SAVE=rescapa_high_strength
+L=5
+python $BIN --plot 1 --strength 0.1 --ntrials $N --virtuals $V --taskname $TASK --layers $L --nproc 51 --maxd 200 --savedir $SAVE
+python $BIN --plot 1 --strength 0.3 --ntrials $N --virtuals $V --taskname $TASK --layers $L --nproc 51 --maxd 200 --savedir $SAVE
+python $BIN --plot 1 --strength 0.7 --ntrials $N --virtuals $V --taskname $TASK --layers $L --nproc 51 --maxd 200 --savedir $SAVE
