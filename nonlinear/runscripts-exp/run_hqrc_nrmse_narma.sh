e31b53c635909a8377f981b262a09c51b39327f8
OminiaVincit <k09tranhoan@gmail.com>
2021-04-23 23:41:51 +0900

    Modify narma hqrc

--- nonlinear/runscripts-exp/run_hqrc_nrmse_narma.sh
+++ nonlinear/runscripts-exp/run_hqrc_nrmse_narma.sh
@@ -3,18 +3,18 @@
 export OMP_NUM_THREADS=1
 
 BIN=../source/narma_hqrc.py
-TRAIN=200
-VAL=200
-T=200
+TRAIN=2000
+VAL=2000
+T=2000
 UNITS=5
 
 SAVE=../../../data/hqrc/narma_nrmse_alpha
 QR='1,2,3,4,5'
-N=1
-V=2
+N=10
+V=20
 #TAU='0.0,1.0,2.0,3.0,4.0'
-TAUS='0.0,1.0'
-ALPHA='0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0'
+TAUS='8.0'
+ALPHA='0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.91,0.92,0.93,0.94,0.95,0.96,0.97,0.98,0.99,0.999,0.9999,1.0'
 
 SOLVER='ridge_pinv'
 for DEEP in 0 1
