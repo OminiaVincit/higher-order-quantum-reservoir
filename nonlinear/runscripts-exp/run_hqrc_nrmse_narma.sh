d6ad025ff2774df5a74709d6fd8e215715cee486
OminiaVincit <k09tranhoan@gmail.com>
2021-04-24 00:11:48 +0900

    plot NRMSE strength

--- nonlinear/runscripts-exp/run_hqrc_nrmse_narma.sh
+++ nonlinear/runscripts-exp/run_hqrc_nrmse_narma.sh
@@ -8,12 +8,12 @@ VAL=2000
 T=2000
 UNITS=5
 
-SAVE=../../../data/hqrc/narma_nrmse_alpha
+SAVE=../../../data/hqrc/narma_nrmse_alpha_tau_8
 QR='1,2,3,4,5'
 N=10
 V=20
 #TAU='0.0,1.0,2.0,3.0,4.0'
-TAUS='8.0'
+TAUS='3.0' #2**x for x in TAUS
 ALPHA='0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.91,0.92,0.93,0.94,0.95,0.96,0.97,0.98,0.99,0.999,0.9999,1.0'
 
 SOLVER='ridge_pinv'
