0335bc22228d7d39ef58cc159a84e215f49d7766
zoro-hpc1 <k09tranhoan@gmail.com>
2021-04-28 01:03:10 +0900

    Add code to plot NARMA vs tau

--- nonlinear/runscripts-exp/run_hqrc_nrmse_narma_tau.sh
+++ nonlinear/runscripts-exp/run_hqrc_nrmse_narma_tau.sh
@@ -12,15 +12,17 @@ SOLVER='ridge_pinv'
 SAVE=/data/zoro/hqrc/narma_nrmse_tau
 QR='5'
 N=10
-V=5
 #TAUS='0.0,1.0,2.0,3.0,4.0'
 #TAUS=\'-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7\'
 TAUS='default'
-for DEEP in 0 1
+for V in 15 20 10
+do
+for DEEP in 0
 do
 for ALPHA in 0.0 0.5 0.9
 do
 python $BIN --deep $DEEP --units $UNITS --strengths $ALPHA --solver $SOLVER --savedir $SAVE --trainlen $TRAIN --vallen $VAL --transient $T --nqrc $QR --ntrials $N --virtuals $V --taudelta $TAUS
+done
 done
 done
 #SOLVER='linear_pinv'
