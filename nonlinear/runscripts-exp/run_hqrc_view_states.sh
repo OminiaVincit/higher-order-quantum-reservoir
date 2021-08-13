1039693414c3a413cfd319ed71249e734bdc4fc2
zoro-hpc1 <k09tranhoan@gmail.com>
2021-08-13 18:24:35 +0900

    Update script to run view states

--- nonlinear/runscripts-exp/run_hqrc_view_states.sh
+++ nonlinear/runscripts-exp/run_hqrc_view_states.sh
@@ -6,24 +6,26 @@ BIN=../source/view_states.py
 LENGTH=500
 BG=400
 ED=500
-SAVE=../../../data/hqrc/dynamics_norm_feed
+SAVE=../../../data/hqrc/dynamics_norm_feed_bw_input
+#SAVE=zoro/data/hqrc/dynamics_norm_feed_bw_input
+
 QR=5
 PROC=100
 CONST=0
 INT=0.05
 SM=0
-CB=1
+CB=0
 SP=1.0
 TP=0
 TS=0.0
 
 for SC in 1.0
 do
-for MS in 0
+for MS in 1
 do
 for SG in 1.0
 do
-for ALPHA in 0.99
+for ALPHA in 1.0
 do
 python $BIN --trans_input $TS --scale_input $SC --type_input $TP --mask_input $MS --combine_input $CB --sigma_input $SG --sparsity $SP --nonlinear $SM --interval $INT --const $CONST --savedir $SAVE --length $LENGTH --bg $BG --ed $ED --nqrc $QR --strength $ALPHA --nproc $PROC
 done
