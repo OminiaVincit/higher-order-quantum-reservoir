35fc302f04defb1220f0aa394278c673bcdb6493
zoro-hpc1 <k09tranhoan@gmail.com>
2021-08-23 13:19:55 +0900

    Modify IPC for multiplexing

---
+++ nonlinear/postprocess/plot_IPC_tau.py
@@ -0,0 +1,147 @@
+import sys
+import os
+import glob
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import plot_utils as putils
+
+if __name__  == '__main__':
+    # Check for command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--parent', type=str, required=True)
+    parser.add_argument('--folders', type=str, required=True)
+    parser.add_argument('--dynamic', type=str, default='full_random')
+    parser.add_argument('--virtuals', type=str, default='1', help='Number of virtual nodes')
+    parser.add_argument('--taus', type=str, default='8.0', help='Taus')
+    parser.add_argument('--nqrc', type=int, default=5)
+
+    parser.add_argument('--thres', type=float, default=0.0)
+    parser.add_argument('--width', type=float, default=0.1)
+    parser.add_argument('--max_capa', type=float, default=4.0)
+    
+    parser.add_argument('--nspins', type=int, default=5, help='Number of spins')
+    parser.add_argument('--max_energy', type=float, default=1.0)
+    parser.add_argument('--alpha', type=float, default=1.0, help='Feedback strength alpha')
+    
+    parser.add_argument('--prefix', type=str, default='ipc_capa')
+    parser.add_argument('--T', type=int, default=200000, help='Number of time steps')
+    parser.add_argument('--keystr', type=str, default='mdeg_4_mvar_4')
+    parser.add_argument('--solver', type=str, default='linear_pinv', help='linear_pinv')
+ 
+    args = parser.parse_args()
+    print(args)
+    parent, folders, dynamic, prefix, keystr, thres, width = args.parent, args.folders, args.dynamic, args.prefix, args.keystr, args.thres, args.width
+    V, tau, nspins, max_energy = args.virtuals, args.taus, args.nspins, args.max_energy
+    T, nqrc, solver = args.T, args.nqrc, args.solver
+    alpha = args.alpha
+
+    posfix  = 'alpha_{:.3f}_V_{}_hqrc_IPC_{}_{}_nqrc_{}_nspins_{}'.format(alpha, V, dynamic, solver, nqrc, nspins)
+    print(posfix)
+    taus = list(np.arange(-7, 7.1, 0.02))
+    taus = [float(2**x) for x in taus]
+
+    folders = [str(x) for x in folders.split(',')]
+    degcapa_ls = []
+    for folder in folders:
+        folder = os.path.join(parent, folder)
+        print('Folder={}'.format(folder))
+        if os.path.isdir(folder) == False:
+            continue
+        dfolder = os.path.join(folder, 'ipc')
+        degcapa, xs = [], []
+        for tB in taus:
+            tarr = []
+            pattern = '{}/{}_tau_{:.6f}_{}*{}*T_{}*.pickle'.format(dfolder, prefix, tB, posfix, keystr, T)
+            filenames = glob.glob(pattern)
+            #print(pattern1)
+            for filename in filenames:
+                #print(filename)
+                with open(filename, "rb") as rfile:
+                    data = pickle.load(rfile)
+                    ipc_arr = data['ipc_arr']
+                    for deg in sorted(ipc_arr.keys()):
+                        darr = ipc_arr[deg].ravel()
+                        tarr.append( np.sum(darr[darr >= thres]) )    
+                #print(deg_arr.shape)
+                degcapa.append(np.array(tarr).ravel())
+                xs.append(tB)
+                break
+        if len(degcapa) == 0:
+            continue
+        degcapa = np.array(degcapa).T
+        degcapa_ls.append(degcapa)
+    
+    degcapa_ls = np.array(degcapa_ls)
+    print(degcapa_ls.shape, len(xs))
+    degcapa_mean = np.mean(degcapa_ls, axis=0)
+    degcapa_std  = np.std(degcapa_ls, axis=0)
+
+    print(degcapa_mean.shape)
+
+    sum_by_cols = np.sum(degcapa_mean, axis=0)
+    
+    fig = plt.figure(figsize=(20, 6), dpi=600)
+    cmap = plt.get_cmap('nipy_spectral')
+    putils.setPlot(fontsize=20, labelsize=16)
+    colors = putils.cycle
+    d_colors = putils.d_colors
+
+    N = min(len(d_colors), degcapa_mean.shape[0])
+
+    ax1 = plt.subplot2grid((1,1), (0,0), colspan=1, rowspan=1)
+    #ax1.set_title('THRES_{}_{}'.format(thres, posfix), size=14)
+
+    ax1.bar(xs, degcapa_mean[0], width=width, color=d_colors[0], edgecolor='gray', label='deg-0')
+    #ax2.bar(xs, degcapa[0] / sum_by_cols,  width=width, color=d_colors[0], edgecolor='k', label='deg-0')
+    for i in range(1, N):
+        bt = degcapa_mean[:i].reshape(i, -1)
+        bt = np.sum(bt, axis=0).ravel()
+        ax1.bar(xs, degcapa_mean[i], bottom=bt, width=width, label='deg-{}'.format(i), color=d_colors[i], edgecolor='gray')
+        #ax2.bar(xs, degcapa[i] / sum_by_cols, bottom=bt/sum_by_cols, width=width, label='deg-{}'.format(i), color=d_colors[i], edgecolor='k')
+    # m_avg, m_std = dict(), dict()
+    # m_avg[0], m_std[0] = degcapa_mean[1], degcapa_std[1]
+    # m_avg[1], m_std[1] = np.sum(degcapa_mean[2:], axis=0), np.sum(degcapa_std[2:], axis=0)
+    #m_avg[2], m_std[2] = degcapa_mean[3] + degcapa_mean[4], degcapa_std[3] + degcapa_std[4]
+    
+    
+    ax1.set_xlabel('$\\tau$', size=32)
+    ax1.set_ylabel('Total IPC', fontsize=28)
+    ax1.set_xscale('log', basex=2)
+    ax1.set_xlim([taus[0], taus[-1]])
+    #ax1.set_xticks(np.linspace(amin, amax, 6))
+    #ax1.set_xticks(list(range(int(amin), int(amax)+1)))
+
+    # ax2.set_xlabel('$\\alpha$', size=32)
+    # ax2.set_ylabel('$C(d)$', size=32)
+    # ax2.set_xlim([amin, amax])
+    # #ax2.set_xscale('log',basex=10)
+    # #ax2.set_ylim([0.0, np.max(m_avg[0]+m_std[0])])
+    # ax2.set_ylim([0.0, args.max_capa])
+    # ax2.set_xticks(np.linspace(amin, amax, 6))
+    
+    #ax1.set_ylim([0, 4.0])
+    #ax1.set_xscale('log', basex=2)
+    ax1.axhline(y=args.max_capa, color='k', linestyle='-')
+
+    ax1.legend()
+    #ax2.legend()
+    
+    for ax in [ax1]:
+        #ax.minorticks_on()
+        ax.tick_params('both', length=8, width=1, which='major', labelsize=28)
+        #ax.tick_params('both', length=4, width=1, which='minor')
+
+    plt.tight_layout()
+
+    fig_folder = os.path.join(folder, 'figs')
+    if os.path.isdir(fig_folder) == False:
+        os.mkdir(fig_folder)
+    outbase = os.path.join(fig_folder, 'fig_thres_{}_{}'.format(thres, posfix))
+    for ftype in ['png', 'svg']:
+        plt.savefig('{}.{}'.format(outbase, ftype), bbox_inches='tight', dpi=600)
+    plt.show()
+
+
+
--- nonlinear/source/runIPC_taus.py
+++ nonlinear/source/runIPC_taus.py
@@ -10,9 +10,6 @@ import os
 import scipy
 import argparse
 import multiprocessing
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib import ticker
 import time
 import datetime
 import hqrc as hqrc
@@ -22,13 +19,14 @@ import utils
 from utils import *
 from IPC import IPCParams
 
-def IPC_compute(qparams, ipcparams, length, ntrials, ranseed, log_filename, savedir, posfix, nqrc, tauls, alpha):
+def IPC_compute(qparams, ipcparams, length, ntrials, ranseed, log_filename, savedir, posfix, nqrc, tauls, alpha, mask_input, combine_input, qr_input):
     logger = get_module_logger(__name__, log_filename)
     logger.info(log_filename)
     
     for tau in tauls:
         qparams.tau = tau
         hqrc.get_IPC(qparams, ipcparams, length, nqrc=nqrc, gamma=alpha, logger=logger, ranseed=ranseed, Ntrials=ntrials, \
+            nonlinear=0, dim_input=qr_input, mask_input=mask_input, combine_input=combine_input,\
             savedir=savedir, posfix='capa_tau_{:.6f}_{}'.format(qparams.tau, posfix), \
             type_input=1, label='tau_{:.6f}'.format(tau))
     
@@ -56,7 +54,11 @@ if __name__  == '__main__':
     parser.add_argument('--writedelay', type=bool, default=False)
     parser.add_argument('--virtuals', type=str, default='1')
     parser.add_argument('--taus', type=str, default='8.0')
-    parser.add_argument('--alphas', type=str, default='0.0')
+    parser.add_argument('--alpha', type=float, default='0.0')
+
+    parser.add_argument('--mask_input', type=int, default=0)
+    parser.add_argument('--combine_input', type=int, default=1)
+    parser.add_argument('--qr_input', type=int, default=1, help='Number of QRs using for input')
 
     parser.add_argument('--dynamic', type=str, default='full_random', help='full_random,half_random,full_const_trans,full_const_coeff,ion_trap,phase_trans')
     parser.add_argument('--basename', type=str, default='hqrc_IPC')
@@ -71,6 +73,7 @@ if __name__  == '__main__':
     max_delay, max_deg, max_num_var, thres, chunk = args.max_delay, args.max_deg, args.max_num_var, args.thres, args.chunk
     max_window, writedelay = args.max_window, args.writedelay
     deg_delays = [int(x) for x in args.deg_delays.split(',')]
+    mask_input, combine_input, qr_input = args.mask_input, args.combine_input, args.qr_input
 
     solver, dynamic = args.solver, args.dynamic
     length, ranseed = args.length, args.seed
@@ -86,8 +89,9 @@ if __name__  == '__main__':
     if os.path.isdir(logdir) == False:
         os.mkdir(logdir)
     
-    basename = '{}_{}_{}_nqrc_{}_nspins_{}_seed_{}_mdeg_{}_mvar_{}_thres_{}_delays_{}_T_{}'.format(\
-        basename, dynamic, solver, nqrc, n_spins, ranseed, max_deg, max_num_var, thres, args.deg_delays, length)
+    basename = '{}_{}_{}_nqrc_{}_nspins_{}_seed_{}_mdeg_{}_mvar_{}_thres_{}_delays_{}_T_{}_mask_{}_cb_{}_qrin_{}'.format(\
+        basename, dynamic, solver, nqrc, n_spins, ranseed, max_deg, max_num_var, thres, args.deg_delays, length,\
+        mask_input, combine_input, qr_input)
     log_filename = os.path.join(logdir, '{}.log'.format(basename))
     logger = get_module_logger(__name__, log_filename)
     logger.info(log_filename)
@@ -98,8 +102,7 @@ if __name__  == '__main__':
     
     
     virtuals = [int(x) for x in args.virtuals.split(',')]
-    alphas = [float(x) for x in args.alphas.split(',')]
-    
+    alpha = args.alpha
     #taus = [float(x) for x in args.taus.split(',')]
     taus = list(np.arange(-7, 7.1, 0.02))
     taus = [float(2**x) for x in taus]
@@ -108,31 +111,31 @@ if __name__  == '__main__':
 
     if os.path.isfile(savedir) == False:
         for V in virtuals:
-            for alpha in alphas:
-                posfix = 'alpha_{:.3f}_V_{}_{}'.format(alpha, V, basename)
-                jobs, pipels = [], []
-                for pid in range(nproc):
-                    tBs = lst[pid]
-                    log_filename = os.path.join(logdir, 'taus_{:.6f}_{}.log'.format(tBs[0], posfix))
-                    
-                    # check file
-                    # degfile = os.path.join(savedir, 'degree_{}.txt'.format(posfix))
-                    # if os.path.isfile(degfile) == True:
-                    #     continue
-                    qparams = QRCParams(n_units=n_spins-1, n_envs=1, max_energy=max_energy, \
-                                beta=beta, virtual_nodes=V, tau=1.0, init_rho=init_rho, solver=solver, dynamic=dynamic)
-                    p = multiprocessing.Process(target=IPC_compute, \
-                        args=(qparams, ipcparams, length, ntrials, ranseed, log_filename, savedir, posfix, nqrc, tBs, alpha))
-                    jobs.append(p)
-                    #pipels.append(recv_end)
+            posfix = 'alpha_{:.3f}_V_{}_{}'.format(alpha, V, basename)
+            jobs, pipels = [], []
+            for pid in range(nproc):
+                tBs = lst[pid]
+                log_filename = os.path.join(logdir, 'taus_{:.6f}_{}.log'.format(tBs[0], posfix))
+                
+                # check file
+                # degfile = os.path.join(savedir, 'degree_{}.txt'.format(posfix))
+                # if os.path.isfile(degfile) == True:
+                #     continue
+                qparams = QRCParams(n_units=n_spins-1, n_envs=1, max_energy=max_energy, \
+                            beta=beta, virtual_nodes=V, tau=1.0, init_rho=init_rho, solver=solver, dynamic=dynamic)
+                p = multiprocessing.Process(target=IPC_compute, \
+                    args=(qparams, ipcparams, length, ntrials, ranseed, log_filename, savedir, posfix, nqrc, tBs, alpha,\
+                        mask_input, combine_input, qr_input))
+                jobs.append(p)
+                #pipels.append(recv_end)
 
-                # Start the process
-                for p in jobs:
-                    p.start()
+            # Start the process
+            for p in jobs:
+                p.start()
 
-                # Ensure all processes have finished execution
-                for p in jobs:
-                    p.join()
+            # Ensure all processes have finished execution
+            for p in jobs:
+                p.join()
 
-                # Sleep 5s
-                time.sleep(5)
+            # Sleep 5s
+            time.sleep(5)
