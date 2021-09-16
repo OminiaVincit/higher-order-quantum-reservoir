0281538b333fa4d5e5bd141f2d29154628b0712b
ripper <k09tranhoan@gmail.com>
2021-09-16 16:24:00 +0900

    Add script of zpc machine to run narma and capacity

--- nonlinear/runscripts-exp/run_hqrc_nrmse_narma_tau_qubits_zpc.sh
+++ nonlinear/runscripts-exp/run_hqrc_nrmse_narma_tau_qubits_zpc.sh
@@ -14,7 +14,7 @@ N=10
 #TAU='0.0,1.0,2.0,3.0,4.0'
 TAUS='3.0'
 #ALPHA='0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.91,0.92,0.93,0.94,0.95,0.96,0.97,0.98,0.99,0.999,0.9999,0.99999,1.0'
-ALPHA='0.0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0'
+ALPHA='0.0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.91,0.92,0.93,0.94,0.95,0.96,0.97,0.98,0.99,0.999,0.9999,0.99999,1.0'
 
 CB_INPUT=0
 MS_INPUT=0
@@ -28,7 +28,7 @@ SOLVER='linear_pinv'
 
 for SIG_INPUT in 1.0
 do
-for V in 15
+for V in 1 5 10
 do
 python $BIN --qrins $QRINS --bnorm $BNORM --orders $ORDERS --sigma_input $SIG_INPUT --nonlinear $NONLINEAR --type_input $TP_INPUT --mask_input $MS_INPUT --combine_input $CB_INPUT --deep $DEEP --units $UNITS --strengths $ALPHA --solver $SOLVER --savedir $SAVE --trainlen $TRAIN --vallen $VAL --transient $T --nqrc $QR --ntrials $N --virtuals $V --taudelta $TAUS
 done
